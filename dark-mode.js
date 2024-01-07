70c94159dbec94a12a794f5c9162cbde57929054
natanjunges <natanajunges@gmail.com>
2024-01-06 21:07:19 -0300

    Update files

--- dark-mode.js
+++ dark-mode.js
@@ -2,14 +2,14 @@
     const darkMode = window.matchMedia("(prefers-color-scheme: dark)");
 
     if (darkMode.matches) {
-        document.body.setAttribute("data-bs-theme", "dark");
+        document.documentElement.setAttribute("data-bs-theme", "dark");
     }
 
     darkMode.addEventListener("change", function (e) {
         if (e.matches) {
-            document.body.setAttribute("data-bs-theme", "dark");
+            document.documentElement.setAttribute("data-bs-theme", "dark");
         } else {
-            document.body.removeAttribute("data-bs-theme");
+            document.documentElement.removeAttribute("data-bs-theme");
         }
     });
 })();
