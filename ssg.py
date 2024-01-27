c605ad1fc187988e9d815548d4a977c22f243e06
natanjunges <natanajunges@gmail.com>
2024-01-27 14:02:46 -0300

    Generate procedurally the blog index page, add post component template, separate templates and components into sections that can be selected

--- ssg.py
+++ ssg.py
@@ -8,7 +8,7 @@ def component_name() -> str:
     return os.path.splitext(os.path.basename(cog.inFile))[0]
 
 
-def generate_component(filename: str, **kwargs: str) -> str:
+def generate_component(filename: str, sections: list[int] = [], **kwargs: str) -> str:
     args = ["cog", "-I", os.path.dirname(__file__)]
 
     for arg in kwargs.items():
@@ -16,15 +16,23 @@ def generate_component(filename: str, **kwargs: str) -> str:
         args.append(f"{arg[0]}={arg[1]}")
 
     args.append(filename)
-    return (
-        subprocess.run(args, capture_output=True, check=True, encoding="UTF-8")
-        .stdout.replace("<!--[[[cog", "<!--")
-        .replace("<!--[[[end]]]-->", "<!---->")
-        .replace("]]]-->", "-->")
-        .rstrip()
-    )
+    component = subprocess.run(args, capture_output=True, check=True, encoding="UTF-8").stdout
+
+    if len(sections) == 0:
+        component = component.replace("<!--***-->\n", "<!--*-->\n")
+    else:
+        component = "<!--*-->\n".join(map(component.split("<!--***-->\n").__getitem__, sections))
+
+    return component.replace("<!--[[[cog", "<!--").replace("<!--[[[end]]]-->", "<!---->").replace("]]]-->", "-->").rstrip()
 
 
-def format_template(filename: str, **kwargs: str) -> None:
+def format_template(filename: str, sections: list[int] = [], **kwargs: str) -> None:
     with open(filename, encoding="UTF-8") as file:
-        cog.out(file.read().format(**kwargs))
+        template = file.read()
+
+        if len(sections) == 0:
+            template = template.replace("<!--***-->\n", "<!--*-->\n")
+        else:
+            template = "<!--*-->\n".join(map(template.split("<!--***-->\n").__getitem__, sections))
+
+        cog.out(template.format(**kwargs))
