8b2b29737ab449df6687f0b43c1175ce9c305ab3
natanjunges <natanajunges@gmail.com>
2024-01-29 15:13:27 -0300

    Optimize loading of static components

--- ssg.py
+++ ssg.py
@@ -4,6 +4,15 @@ import subprocess
 import cog
 
 
+def _split_join(input_: str, sections: list[int] = []) -> str:
+    if len(sections) == 0:
+        input_ = input_.replace("<!--***-->\n", "")
+    else:
+        input_ = "".join(map(input_.split("<!--***-->\n").__getitem__, sections))
+
+    return input_
+
+
 def component_name() -> str:
     return os.path.splitext(os.path.basename(cog.inFile))[0]
 
@@ -17,22 +26,15 @@ def generate_component(filename: str, sections: list[int] = [], **kwargs: str) -
 
     args.append(filename)
     component = subprocess.run(args, capture_output=True, check=True, encoding="UTF-8").stdout
+    return _split_join(component, sections).rstrip()
 
-    if len(sections) == 0:
-        component = component.replace("<!--***-->\n", "")
-    else:
-        component = "".join(map(component.split("<!--***-->\n").__getitem__, sections))
 
-    return component.rstrip()
+def load_component(filename: str, sections: list[int] = []) -> str:
+     with open(filename, encoding="UTF-8") as file:
+        return _split_join(file.read(), sections).rstrip()
 
 
 def format_template(filename: str, sections: list[int] = [], **kwargs: str) -> None:
     with open(filename, encoding="UTF-8") as file:
-        template = file.read()
-
-        if len(sections) == 0:
-            template = template.replace("<!--***-->\n", "")
-        else:
-            template = "".join(map(template.split("<!--***-->\n").__getitem__, sections))
-
+        template = _split_join(file.read(), sections)
         cog.out(eval(f'f"""{template}"""', kwargs))
