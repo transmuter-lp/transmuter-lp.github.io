f6371476ae1df6d966f9f7fb3306db479f9c0e32
natanjunges <natanajunges@gmail.com>
2024-04-22 19:37:05 -0300

    Simplify posts by using _post as component

--- ssg.py
+++ ssg.py
@@ -7,6 +7,7 @@ import cog
 def name() -> str:
     return os.path.splitext(os.path.basename(cog.inFile))[0]
 
+
 def init(filename: str, **kwargs: str) -> str:
     args = ["cog", "-d", "-I", os.path.dirname(__file__), "-p", "import ssg; init = True"]
 
@@ -17,9 +18,11 @@ def init(filename: str, **kwargs: str) -> str:
     args.append(filename)
     return subprocess.run(args, capture_output=True, check=True, encoding="UTF-8").stdout.rstrip()
 
+
 def text(filename: str) -> str:
     with open(filename, encoding="UTF-8") as file:
         return file.read().rstrip()
+
 
 def format(**kwargs: str) -> None:
     cog.out(eval(f'f"""{cog.previous}"""', kwargs))
